e0e24a123844467bdec64e759ed28134e1b546b4
Stephen Magill <stephen@muse.dev>
2020-06-23 23:48:38 -0400

    Fix resource leak

--- webgoat-lessons/challenge/src/main/java/org/owasp/webgoat/challenges/challenge7/MD5.java
+++ webgoat-lessons/challenge/src/main/java/org/owasp/webgoat/challenges/challenge7/MD5.java
@@ -165,10 +165,11 @@ public class MD5 {
      * @since ostermillerutils 1.00.00
      */
     public static byte[] getHash(File f) throws IOException {
-        InputStream is = new FileInputStream(f);
-        byte[] hash = getHash(is);
-        is.close();
-        return hash;
+        try (InputStream is = new FileInputStream(f)) {
+            byte[] hash = getHash(is);
+            is.close();
+            return hash;    
+        }
     }
 
     /**
